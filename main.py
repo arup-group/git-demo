5daa020ac5f98b7642b4118d54c54605e63fa1b3
Jared Stock <jared.stock@arup.com>
2021-09-01 16:42:49 -0400

    Update for more complicated functions

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+import math
+
+
 def hello(name: str) -> str:
     """
     A hello world function to say hello to a user
@@ -10,7 +13,24 @@ def hello(name: str) -> str:
     return f'Hello, {name}!'
 
 
+def pythagorean(a: float, b: float) -> float:
+    """
+
+    :param a: one side of a triangle
+    :param b: the other side of a triangle
+    :return: the length of the hypotenuse of the triangle
+    :rtype: float
+    """
+    if a > 0 and b > 0:
+        return math.sqrt(a ** 2 + b ** 2)
+    else:
+        raise Exception('Values must be greater than zero!!')
+
+
 if __name__ == '__main__':
-    name = input("What's your name? ")
-    res = hello(name)
+    # name = input("What's your name? ")
+    # res = hello(name)
+    # print(res)
+
+    res = pythagorean(3, '4')
     print(res)
--- test.py
+++ test.py
@@ -4,12 +4,26 @@ import main
 
 class MyTestCase(unittest.TestCase):
     def test_trivial(self):
-        self.assertEqual(True, False)
+        self.assertEqual(True, True)
 
     def test_main(self):
         test_data = 'Test Name'
         res = main.hello(test_data)
         self.assertTrue(type(res) == str)
+
+    def test_pythagorean(self):
+        res = main.pythagorean(3, 4)
+
+        self.assertTrue(type(res) == float)
+
+        self.assertEqual(res, 5)
+
+    def test_pythagorean_errors(self):
+        with self.assertRaises(Exception):
+            main.pythagorean(3, 0)
+
+        with self.assertRaises(TypeError):
+            main.pythagorean(3, '4')
 
 
 if __name__ == '__main__':
